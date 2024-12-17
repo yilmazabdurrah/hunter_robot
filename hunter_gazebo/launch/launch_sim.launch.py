1eac2684050ed61e69b4476b5b726a7c21590100
ibrahim <ibrahim.hroub7@gmail.com>
2024-12-17 19:41:53 +0000

    added use sim time param

--- hunter_gazebo/launch/launch_sim.launch.py
+++ hunter_gazebo/launch/launch_sim.launch.py
@@ -9,25 +9,33 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterValue
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument
 
 from ament_index_python.packages import get_package_share_directory
 
-
 def generate_launch_description():
-    gazebo_params_file = os.path.join(get_package_share_directory("hunter_gazebo"),'config','gazebo_params.yaml')
+    # Declare the use_sim_time argument
+    use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation time'
+    )
+
+    # Launch configuration for use_sim_time
+    use_sim_time_config = LaunchConfiguration('use_sim_time')
+
+    gazebo_params_file = os.path.join(get_package_share_directory("hunter_gazebo"), 'config', 'gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+    )
 
     hunter_description_path = os.path.join(
         get_package_share_directory('hunter_description'))
-
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -43,18 +51,20 @@ def generate_launch_description():
         "robot_description": ParameterValue(robot_description_content, value_type=str)
     }
 
-
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[robot_description]
+        parameters=[robot_description, {'use_sim_time': use_sim_time_config}]
     )
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'hunter'],
-                        output='screen')
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'hunter'],
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time_config}]
+    )
 
     load_joint_state_broadcaster = ExecuteProcess(
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
@@ -76,9 +86,11 @@ def generate_launch_description():
             os.path.join(hunter_description_path, 'rviz/robot_view.rviz'),
         ],
         output='screen',
+        parameters=[{'use_sim_time': use_sim_time_config}]
     )
 
     return LaunchDescription([
+        use_sim_time,
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=spawn_entity,
