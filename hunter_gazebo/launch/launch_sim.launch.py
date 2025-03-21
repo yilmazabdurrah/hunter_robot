e1f638ea7e997a039097a02102db643a248e9df7
ibrahim <ibrahim.hroub7@gmail.com>
2025-03-21 13:45:01 +0000

    add real ackermann controller and update dynamics parameters

--- hunter_gazebo/launch/launch_sim.launch.py
+++ hunter_gazebo/launch/launch_sim.launch.py
@@ -72,9 +72,9 @@ def generate_launch_description():
         output='screen'
     )
 
-    load_tricycle_controller = ExecuteProcess(
+    load_ackermann_controller = ExecuteProcess(
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'ackermann_like_controller'],
+             'ackermann_controller'],
         output='screen'
     )
 
@@ -100,7 +100,7 @@ def generate_launch_description():
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=load_joint_state_broadcaster,
-                on_exit=[load_tricycle_controller],
+                on_exit=[load_ackermann_controller],
             )
         ),
         gazebo,
